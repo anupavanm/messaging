7ec90751d3f924ad1431227c733ab6a8af775892
Anupavan.Muddana <anupavanm@gmail.com>
2016-09-22 11:35:25 -0400

    newly added

---
+++ ADAPItems/ADAPItems/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ADAPItems
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ADAPItems/ADAPItems/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ADAPItems.Startup))]
+namespace ADAPItems
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
